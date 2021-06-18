33bacc9880d5ed6b1935848caff2b9aab460a6be
Y-poi-sir <66407413+Y-poi-sir@users.noreply.github.com>
2021-06-18 20:08:02 +0800

    Add files via upload

---
+++ WindowsFormsApp-Cal/WindowsFormsApp-Cal/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp_Cal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Click = new System.Windows.Forms.Button();
+            this.textBox_num1 = new System.Windows.Forms.TextBox();
+            this.textBox_num2 = new System.Windows.Forms.TextBox();
+            this.comboBox_sign = new System.Windows.Forms.ComboBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_Click
+            // 
+            this.button_Click.Location = new System.Drawing.Point(353, 274);
+            this.button_Click.Name = "button_Click";
+            this.button_Click.Size = new System.Drawing.Size(141, 58);
+            this.button_Click.TabIndex = 0;
+            this.button_Click.Text = "计算";
+            this.button_Click.UseVisualStyleBackColor = true;
+            this.button_Click.Click += new System.EventHandler(this.button_Click_Click);
+            // 
+            // textBox_num1
+            // 
+            this.textBox_num1.Location = new System.Drawing.Point(353, 57);
+            this.textBox_num1.Name = "textBox_num1";
+            this.textBox_num1.Size = new System.Drawing.Size(141, 25);
+            this.textBox_num1.TabIndex = 1;
+            // 
+            // textBox_num2
+            // 
+            this.textBox_num2.Location = new System.Drawing.Point(353, 132);
+            this.textBox_num2.Name = "textBox_num2";
+            this.textBox_num2.Size = new System.Drawing.Size(141, 25);
+            this.textBox_num2.TabIndex = 2;
+            // 
+            // comboBox_sign
+            // 
+            this.comboBox_sign.FormattingEnabled = true;
+            this.comboBox_sign.Location = new System.Drawing.Point(353, 206);
+            this.comboBox_sign.Name = "comboBox_sign";
+            this.comboBox_sign.Size = new System.Drawing.Size(141, 23);
+            this.comboBox_sign.TabIndex = 3;
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(353, 387);
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(141, 25);
+            this.textBox_result.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(269, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "数字1：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "数字2：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "选择运算符：";
+
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 397);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "结果：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.comboBox_sign);
+            this.Controls.Add(this.textBox_num2);
+            this.Controls.Add(this.textBox_num1);
+            this.Controls.Add(this.button_Click);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Click;
+        private System.Windows.Forms.TextBox textBox_num1;
+        private System.Windows.Forms.TextBox textBox_num2;
+        private System.Windows.Forms.ComboBox comboBox_sign;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp-Cal/WindowsFormsApp-Cal/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Cal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox_sign.Items.Add("+");
+            comboBox_sign.Items.Add("-");
+            comboBox_sign.Items.Add("*");
+            comboBox_sign.Items.Add("/");
+        }
+
+
+        private void button_Click_Click(object sender, EventArgs e)
+        {
+            double a = 0, b = 0;
+            try
+            {
+                a = double.Parse(textBox_num1.Text);
+                b = double.Parse(textBox_num2.Text);
+
+                switch (comboBox_sign.Text)
+                {
+                    case "+":
+                        textBox_result.Text = Convert.ToString(a + b);
+                        break;
+                    case "-":
+                        textBox_result.Text = Convert.ToString(a - b);
+                        break;
+                    case "*":
+                        textBox_result.Text = Convert.ToString(a * b);
+                        break;
+                    case "/":
+                        if (b != 0)
+                            textBox_result.Text = Convert.ToString(a / b);
+                        else
+                            textBox_result.Text = "除0错误";
+                        break;
+                    default:
+                        textBox_result.Text = "输入符号错误";
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                textBox_result.Text = "输入内容不是数字";
+            }
+            catch (OverflowException)
+            {
+                textBox_result.Text = "溢出";
+            }
+        }
+    }
+}
